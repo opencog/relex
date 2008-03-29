bdf18013c38573c1651f484deb20e68697946367
Linas Vepstas <linasvepstas@gmail.com>
2008-03-29 14:45:04 -0500

    start filling things in

--- src/java/relex/tree/PatternMatch.java
+++ src/java/relex/tree/PatternMatch.java
@@ -15,6 +15,8 @@
  */
 package relex.tree;
 
+import relex.feature.FeatureNode;
+
 /**
  * Implements phrase pattern matching.
  *
@@ -40,25 +42,75 @@ public class PatternMatch
 	 */
 	public static Boolean match (String pattern, PhraseTree pt, PatternCallback cb)
 	{
+		return _match(pattern, pt, cb);
+	}
+
+	/**
+	 * Return true to indicate mismatch
+	 */
+	private static Boolean _match (String pattern, PhraseTree pt, PatternCallback cb)
+	{
+System.out.println("enter match pat= " + pattern + " tree=" + pt.toString());
 		int open = pattern.indexOf('(');
-		if (open < 0) return false;  // no opening paren was found.
+		if (open < 0) return true;  // no opening paren was found.
 
 		int close = pattern.lastIndexOf(')');
-		if (close < 0) return false;  // no closing paren was found.
+		if (close < 0) return true;  // no closing paren was found.
 
 		pattern = pattern.substring(open+1, close).trim();
 
 		int white = pattern.indexOf(' ');
-		if (white < 0) return false;  // no whitespace after token!
+		if (white < 0) return true;  // no whitespace after token!
 
 		// ptype is the phrase type (S, NP, VP, ADVP, etc.)
 		String ptype = pattern.substring(0, white);
 
+		// phrase types must match.
 		String phtype = pt.getPhraseType();
-System.out.println ("duude ptype="+ptype + " ph=" + phtype);
+		// System.out.println ("ptype="+ptype + " phrasetype=" + phtype);
+		if (!ptype.equals(phtype)) return true;
 
-		if (!ptype.equals(phtype)) return false;
+		// skip over the type in the pattern string.
+		pattern = pattern.substring(white).trim();
+		boolean pat_starts_with_word = !pattern.startsWith("(");
 
+		// Now start walking the thing.
+		FeatureNode fn = pt.get("phr-head");
+		boolean saw_word = false;
+System.out.println("duude so far so good with " + pt.toString() + " and pat=" + pattern);
+		while (fn != null)
+		{
+			FeatureNode wd = fn.get("phr-word");
+			if (wd != null)
+			{
+				if (!pat_starts_with_word) return true; // no match
+FeatureNode fu= wd.get("orig_str");
+String fus="";
+if (fu != null) fus=fu.getValue();
+System.out.println("duude got match word "+ fus);
+				saw_word = true;
+			}
+			FeatureNode subf = fn.get("phr-head");
+			if (subf != null)
+			{
+				if (pat_starts_with_word && (saw_word == false)) return true; // no match
+				open = pattern.indexOf('(');
+				close = pattern.indexOf(')');
+				String subpat = pattern.substring(open, close+1);
+System.out.println("duude got subf");
+				PhraseTree subt = new PhraseTree(fn);
+				boolean rc = _match(subpat, subt, cb);
+				if (rc) return rc;
+
+				pattern = pattern.substring(close+1).trim();
+				pat_starts_with_word = !pattern.startsWith("(");
+				saw_word = false;
+			}
+			fn = fn.get("phr-next");
+		}
+
+		if (0 < pattern.length() && pat_start_with_word && !saw_word) return true;
+System.out.println("at the end but patt=" + pattern + "=");
 		Boolean rc = cb.PMCallback(pattern, pt);
 		return rc;
 	}
