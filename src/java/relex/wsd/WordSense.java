4e0b12545a6e502959a40f9c9d8872e6707b2aa3
Linas Vepstas <linasvepstas@gmail.com>
2008-04-22 16:10:03 -0500

    initial checkin

---
+++ src/java/relex/wsd/WordSense.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2008 Novamente LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package relex.wsd;
+
+import relex.RelexInfo;
+import relex.stats.SimpleTruthValue;
+
+/**
+ * Experimental word-sense disambiguation code.
+ */
+public class WordSense
+{
+	public SimpleTruthValue wordSenseMatch(String word,
+	                                       RelexInfo example,
+	                                       RelexInfo target)
+	{
+		SimpleTruthValue stv = new SimpleTruthValue();
+
+		return stv;
+	}
+
+} // end WordSense
+
