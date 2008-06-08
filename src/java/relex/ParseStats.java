2133a15e2aea74cd96e17a65eee4be95ae313124
Linas Vepstas <linasvepstas@gmail.com>
2008-06-08 13:39:40 -0500

    add more stats

--- src/java/relex/ParseStats.java
+++ src/java/relex/ParseStats.java
@@ -29,25 +29,61 @@ public class ParseStats
 	private int count;
 	private Histogram parse_count;
 	private Histogram word_count;
+	private int failed_parses;
+	private Histogram first_parse_confidence;
+	private Histogram second_parse_confidence;
+	private Histogram third_parse_confidence;
 
 	public ParseStats()
 	{
 		count = 0;
 		word_count = new Histogram(1,31);
 		parse_count = new Histogram(0,10);
+		failed_parses = 0;
+
+		first_parse_confidence = new Histogram(20, 0.0, 1.0);
+		second_parse_confidence = new Histogram(20, 0.0, 1.0);
+		third_parse_confidence = new Histogram(20, 0.0, 1.0);
 	}
 
 	public void bin(RelexInfo ri)
 	{
 		count ++;
-		parse_count.bin(ri.parsedSentences.size());
+		int nparses = ri.parsedSentences.size();
+		if (nparses <= 0) return;
+
+		parse_count.bin(nparses);
+
+		ParsedSentence fs = ri.parsedSentences.get(0);
+		word_count.bin(fs.getNumWords());
+
+		// If the first parse has skipped words, the parse is "failed"
+		if (fs.getNumSkippedWords() != 0) failed_parses ++;
+
+		// Count the first parse only if its "good"
+		if (fs.getNumSkippedWords() == 0)
+			first_parse_confidence.bin(fs.getTruthValue().getConfidence());
+
+		if (2 <= nparses)
+			second_parse_confidence.bin(ri.parsedSentences.get(1).getTruthValue().getConfidence());
+		if (3 <= nparses)
+			third_parse_confidence.bin(ri.parsedSentences.get(2).getTruthValue().getConfidence());
+
 	}
 
 	public String toString()
 	{
 		String str = "";
-		str += "Total sentences: " + count;
-		str += "Parses per sentence: " + parse_count.getMean();
+		str += "\nTotal sentences: " + count;
+		str += "\nFailed parses: " + failed_parses;
+		str += "\nWords per sentence: " + word_count.getMean();
+		str += "\nParses per sentence: " + parse_count.getMean();
+		str += "\nConfidence of first parse: " + first_parse_confidence.getMean() +
+		       " of " + first_parse_confidence.getCount() + " parses";
+		str += "\nConfidence of second parse: " + second_parse_confidence.getMean() +
+		       " of " + second_parse_confidence.getCount() + " parses";
+		str += "\nConfidence of third parse: " + third_parse_confidence.getMean() +
+		       " of " + third_parse_confidence.getCount() + " parses";
 		return str;
 	}
 }
