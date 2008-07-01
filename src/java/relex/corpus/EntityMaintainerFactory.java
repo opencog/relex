1a33763c44d4cfcb96ae6e072beb64430669ccf7
Linas Vepstas <linasvepstas@gmail.com>
2008-06-30 21:16:34 -0500

    try to clean up at least a little bit.

--- src/java/relex/corpus/EntityMaintainerFactory.java
+++ src/java/relex/corpus/EntityMaintainerFactory.java
@@ -1,35 +1,42 @@
+/*
+ * Copyright 2008 Borislav Iordanov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package relex.corpus;
 
 import relex.entity.EntityMaintainer;
 
 /**
- * 
- * <p>
- * An <code>EntityMaintainerFactory</code> implementation is responsible for
- * detecting named entities in a sentence and producing an instance
- * of {@link EntityMaintainer}.
- * </p>
+ * An EntityMaintainerFactory implementation is responsible for detecting
+ * named entities in a sentence and producing an instance of 
+ * {@link EntityMaintainer}.
  *
- * <p>
- * This class also serves as a factory of a globally configured <code>EntityMaintainerFactory</code>
- * through the <code>relex.corpus.EntityMaintainerFactory</code> system property.  The default
- * value of this property is <code>relex.corpus.GateEntityDetector</code>. This globally
- * configured <code>EntityMaintainerFactory</code> factory is created only once and
- * maintained as a singleton within this class;
- * </p>
- * 
- * @author Borislav Iordanov
- *
+ * This class also serves as a factory of a globally configured 
+ * EntityMaintainerFactory through the 
+ * relex.corpus.EntityMaintainerFactory system property.  The default
+ * value of this property is relex.corpus.GateEntityDetector. This
+ * globally configured EntityMaintainerFactory factory is created only
+ * once and maintained as a singleton within this class.
  */
 public abstract class EntityMaintainerFactory
 {	
 	public static final String DEFAULT_FACTORY = "relex.corpus.GateEntityDetector";
 	
 	/**
-	 * <p>
-	 * Detect named entities in the string <code>sentence</code> and return
-	 * an <code>EntityMaintainer</code>.
-	 * </p>
+	 * Detect named entities in the string sentence and return
+	 * an EntityMaintainer.
 	 */
 	public abstract EntityMaintainer makeEntityMaintainer(String sentence);
 	
