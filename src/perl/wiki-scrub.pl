12b379aa3e6d934ea5092bedd351f7748cc1bc89
Linas Vepstas <linasvepstas@gmail.com>
2008-07-15 09:00:49 -0500

    fix infobox handling, use utf8 encoding, and handle general URL's

--- src/perl/wiki-scrub.pl
+++ src/perl/wiki-scrub.pl
@@ -4,12 +4,22 @@
 # english-language sentences.  This  script removes wiki markup, URL's
 # tables, images, & etc.
 
+# Need to specify the binmodes, in order for \w to match utf8 chars
+use utf8;
+binmode STDIN, ':encoding(UTF-8)'; 
+binmode STDOUT, ':encoding(UTF-8)';
+
 $have_text = 0;
 $have_infobox = 0;
 while (<>)
 {
 	if (/<text /) { $have_text = 1; }
 	if (/<\/text>/) { $have_text = 0; }
+
+	chop;
+
+	# remove the text xml
+	s/.*<text xml:space="preserve">//;
 
 	# kill photo galleries
 	if (/&lt;gallery&gt;/) { $have_text = 0; }
@@ -19,16 +29,17 @@ while (<>)
 	if (/^\{\|/) { $have_text = 0; }
 	if (/^\|\}/) { $have_text = 1; next; }
 
-	if (/\s*\{\{Infobox/) { $have_text = 0; $have_infobox = 1;}
-	if ($have_infobox && /\}\}/) { $have_text = 1; $have_infobox = 0; next; }
+	if (/\s*\{\{Infobox/) { $have_text = 0; $have_infobox = 1; next;}
+	if ($have_infobox && /\{\{/) { $have_infobox++; }
+	if ($have_infobox && /\}\}/) { 
+		$have_infobox--; 
+		if (0 == $have_infobox) {$have_text = 1;}
+		next;
+	}
+	if ($have_infobox) { next; }
 
 	# ignore everything that isn't in a text section.
 	if (0 == $have_text) { next; }
-
-	chop;
-
-	# remove the text xml
-	s/.*<text xml:space="preserve">//;
 
 	# remove bogus markup
 	s/&lt;nowiki&gt;//g;
@@ -38,7 +49,8 @@ while (<>)
 	s/\'\'\'//g;
 	s/\'\'//g;
 
-	# Ignore everything of the form [[en:title]] 
+	# Ignore everything of the form ^[[en:title]] (these are tranlsated
+	# pages)
 	if (/^\[\[\w[\w-]+?:.+?\]\]$/) { next; }
 
 	# Ignore templates i.e. {{template gorp}}
@@ -53,11 +65,12 @@ while (<>)
 	# Kill image tags of the form [[Image:Chemin.png|thumb|300px|blah]]
 	s/\[\[Image:.+?\]\]//g;
 
-	# kill wikilinks of the form [[the real link|The Stand-In Text]]
-	s/\[\[[:\-\w '\(\)]+?\|(.+?)\]\]/$1/g;
+	# kill wikilinks of the form [[the real link#ugh|The Stand-In Text]]
+	# also [[Wikipedia:spical/blah|The Stand-In Text]]
+	s/\[\[[#:,\/\-\w '\(\)]+?\|(.+?)\]\]/$1/g;
 
 	# Kill ordinary links -- [[Stuf more stuff]]
-	s/\[\[([:\-\w '\(\)]+?)\]\]/$1/g;
+	s/\[\[([:,\/\-\w '\(\)]+?)\]\]/$1/g;
 
 	# kill weblinks  i.e. [http:blah.com/whjaterver A Cool Site]
 	s/\[\S+ (.+?)\]/$1/g;
