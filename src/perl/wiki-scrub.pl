5384bdee562b7be06d76a913b4f3b32c7309c5d1
Linas Vepstas <linasvepstas@gmail.com>
2008-07-29 09:40:56 -0500

    updates for scrubbing.

--- src/perl/wiki-scrub.pl
+++ src/perl/wiki-scrub.pl
@@ -179,6 +179,14 @@ while (<>)
 	s/&lt;\/em&gt;//g;
 	s/&lt;tt&gt;//g;
 	s/&lt;\/tt&gt;//g;
+	s/&lt;title&gt;//g;
+	s/&lt;\/title&gt;//g;
+	s/&lt;comment&gt;//g;
+	s/&lt;\/comment&gt;//g;
+	s/&lt;username&gt;//g;
+	s/&lt;\/username&gt;//g;
+	s/&lt;timestamp&gt;//g;
+	s/&lt;\/timestamp&gt;//g;
 	s/&lt;id&gt;//g;
 	s/&lt;\/id&gt;//g;
 	s/&lt;pre&gt;//g;
