8a3d54a8a1f2637aaed70cc5cf56860385dfad69
U-SHAGGY\muriloq <muriloq@shaggy>
2008-09-04 13:01:43 -0300

    Windows files (for building and running)

---
+++ link-grammar-server.bat
@@ -0,0 +1,25 @@
+set GATE_HOME=C:/Progra~1/GATE-4.0
+set DISCO_HOME=C:/disco
+java ^
+-Xmx1024m ^
+-Djava.library.path=%DISCO_HOME%/native/windows ^
+-Drelex.linkparserpath=%DISCO_HOME%/data/linkparser ^
+-Dwordnet.configfile=data/wordnet/file_properties-win32.xml ^
+-Drelex.algpath=data/relex-semantic-algs.txt ^
+-classpath C:\relex\bin;^
+%DISCO_HOME%\lib\link-grammar.jar;^
+%DISCO_HOME%\lib\gnu-getopt.jar;^
+%DISCO_HOME%\lib\wordnet\jwnl.jar;^
+%DISCO_HOME%\lib\wordnet\commons-logging.jar;^
+%DISCO_HOME%\lib\opennlp\opennlp-tools-1.3.0.jar;^
+%DISCO_HOME%\lib\opennlp\maxent-2.4.0.jar;^
+%DISCO_HOME%\lib\opennlp\trove.jar;^
+%GATE_HOME%\bin\gate.jar;^
+%GATE_HOME%\lib\jdom.jar;^
+%GATE_HOME%\lib\xercesImpl.jar;^
+%GATE_HOME%\lib\jasper-compiler-jdt.jar;^
+%GATE_HOME%\lib\nekohtml-0.9.5.jar;^
+%GATE_HOME%\lib\ontotext.jar;^
+%GATE_HOME%\lib\stax-api-1.0.1.jar;^
+%GATE_HOME%\lib\PDFBox-0.7.2.jar ^
+relex.parser.LinkParserServer %1
