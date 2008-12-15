07f08bd4bcf002c8d6ea72138801ae9269a35950
Linas Vepstas <linasvepstas@gmail.com>
2008-12-15 11:33:58 -0600

    update the required version numbers for link-grammar

--- link-grammar-server.sh
+++ link-grammar-server.sh
@@ -4,6 +4,6 @@ export LANG=en_US.UTF-8
 
 VM_OPTS="-Xmx1024m -Djava.library.path=/usr/lib:/usr/local/lib"
 RELEX_OPTS="-Drelex.algpath=data/relex-semantic-algs.txt"
-CLASSPATH="-classpath bin:/usr/share/java/link-grammar-4.4.0.jar:/usr/local/share/java/link-grammar-4.4.0.jar"
+CLASSPATH="-classpath bin:/usr/share/java/link-grammar-4.4.1.jar:/usr/local/share/java/link-grammar-4.4.1.jar"
 
 java $VM_OPTS $RELEX_OPTS $CLASSPATH relex.parser.LinkParserServer $1
