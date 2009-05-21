3d30c9bfede5787cc2cf32d6d09ffba4ccfe6ab1
Linas Vepstas <linasvepstas@gmail.com>
2009-05-21 15:53:00 -0500

    Add copyright boilerplate

--- src/java/relex/parser/IParser.java
+++ src/java/relex/parser/IParser.java
@@ -1,14 +1,27 @@
+/*
+ * Copyright 2009 Borislav Iordanov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	 http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package relex.parser;
 
 import relex.Sentence;
 
 /**
- * 
- * <p>
- * Interface to the parsing stage within RelEx. Implementation are responsible for both
- * parsing a piece of text and converting it to RelEx's <code>FeatureNode</code> 
- * based representation. 
- * </p>
+ * Interface to the parsing stage within RelEx. Implementations are 
+ * responsible for both parsing a piece of text and converting it 
+ * to RelEx's <code>FeatureNode</code> based representation. 
  *
  * @author Borislav Iordanov
  *
@@ -16,21 +29,19 @@ import relex.Sentence;
 public interface IParser
 {
 	/**
-	 * <p>
-	 * Parse a piece of text, usually a single sentence delimited by proper punctuation, convert
-	 * it to RelEx representation and return the result.
-	 * </p>
+	 * Parse a piece of text, usually a single sentence delimited by 
+	 * proper punctuation, convert it to RelEx representation and 
+	 * return the result.
 	 * 
-	 * <p>
 	 * Once this method returns, the parser is available again. 
-	 * </p>
 	 * 
 	 * @param text The text/sentence to parse.
-	 * @return The parse result as a <code>Sentence</code> instance. This method should never
-	 * return <code>null</code>. 
-	 * @throws ParseException Whenever something went wrong during the parsing, such as failure
-	 * to acquire a resource or some such. No exception should be thrown if the text is unparseable.
-	 *   
+	 * @return The parse result as a <code>Sentence</code> instance. 
+	 *         This method should never return <code>null</code>. 
+	 * @throws ParseException Whenever something went wrong during 
+	 *         the parsing, such as failure to acquire a resource
+	 *         or some such. No exception should be thrown if the 
+	 *         text is unparseable.
 	 */
 	Sentence parse(String text) throws ParseException;
 }
