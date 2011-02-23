56787160d1ec46b487d08a0741597d1f919d02bf
Linas Vepstas <linasvepstas@gmail.com>
2011-02-22 20:50:51 -0600

    batch file for moving parsed files

---
+++ mv-files.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# mv-it.sh
+
+# IFS=$(echo -en "\n\b")
+
+FILES=parsed/*/*
+
+mkdir enwiki-20101011
+cd enwiki-20101011
+mkdir parsed
+cd parsed 
+mkdir A
+mkdir B
+mkdir C
+mkdir D
+mkdir E
+mkdir F
+mkdir G
+mkdir H
+mkdir I
+mkdir J
+mkdir K
+mkdir L
+mkdir M
+mkdir N
+mkdir O
+mkdir P
+mkdir Q
+mkdir R
+mkdir S
+mkdir T
+mkdir U
+mkdir V
+mkdir W
+mkdir X
+mkdir Y
+mkdir Z
+mkdir num
+mkdir misc
+
+cd ../..
+
+
+cnt=0;
+for fpath in $FILES
+do
+	f=${fpath##*/}
+	
+	# echo  "$fpath"
+	mv "${fpath}" "enwiki-20101011/${fpath}"
+	let cnt=cnt+1
+
+	if [ $cnt -gt 40000 ];
+	then 
+		break
+	fi
+	# echo $cnt
+done
+
