e4be4b4ff5e1676df7e03b6f605076be1bdcda0d
Linas Vepstas <linasvepstas@gmail.com>
2012-10-15 14:51:51 -0500

    Bump the version number

--- src/java/relex/Version.java
+++ src/java/relex/Version.java
@@ -20,7 +20,7 @@ package relex;
  */
 public class Version
 {
-	private static final String VERSION = "1.4.0";
+	private static final String VERSION = "1.4.1";
 
 	static public String getVersion()
 	{
