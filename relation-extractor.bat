ab8a4c1ec182ed5142f5faa88380ef505fa8d745
Linas Vepstas <linasvepstas@gmail.com>
2013-10-31 23:50:54 -0500

    minor cleanup of windows batch file

--- relation-extractor.bat
+++ relation-extractor.bat
@@ -1,4 +1,3 @@
-set GATE_HOME=C:/Progra~1/GATE-4.0
 set DISCO_HOME=C:/disco
 
 java ^
@@ -12,16 +11,8 @@ java ^
 %DISCO_HOME%\lib\gnu-getopt.jar;^
 %DISCO_HOME%\lib\wordnet\jwnl.jar;^
 %DISCO_HOME%\lib\wordnet\commons-logging.jar;^
-%DISCO_HOME%\lib\opennlp\opennlp-tools-1.3.0.jar;^
-%DISCO_HOME%\lib\opennlp\maxent-2.4.0.jar;^
+%DISCO_HOME%\lib\opennlp\opennlp-tools-1.5.3.jar;^
+%DISCO_HOME%\lib\opennlp\maxent-3.0.3.jar;^
 %DISCO_HOME%\lib\opennlp\trove.jar;^
-%GATE_HOME%\bin\gate.jar;^
-%GATE_HOME%\lib\jdom.jar;^
-%GATE_HOME%\lib\xercesImpl.jar;^
-%GATE_HOME%\lib\jasper-compiler-jdt.jar;^
-%GATE_HOME%\lib\nekohtml-0.9.5.jar;^
-%GATE_HOME%\lib\ontotext.jar;^
-%GATE_HOME%\lib\stax-api-1.0.1.jar;^
-%GATE_HOME%\lib\PDFBox-0.7.2.jar ^
-relex.RelationExtractor -n 4 -l -t -f -g ^
+relex.RelationExtractor -n 4 -l -t -f ^
 -s "Alice wrote a book about dinosaurs for the University of California in Berkeley."
