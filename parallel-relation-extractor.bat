5edd455e7d64fcef39b480f2a4ae632312079cad
Linas Vepstas <linasvepstas@gmail.com>
2013-11-06 11:11:11 -0600

    strip out more GATE-related crap

--- parallel-relation-extractor.bat
+++ parallel-relation-extractor.bat
@@ -1,4 +1,3 @@
-set GATE_HOME=C:/Progra~1/GATE-4.0
 set DISCO_HOME=C:/disco
 java ^
 -Xmx1024m ^
@@ -14,12 +13,4 @@ java ^
 %DISCO_HOME%\lib\opennlp\opennlp-tools-1.3.0.jar;^
 %DISCO_HOME%\lib\opennlp\maxent-2.4.0.jar;^
 %DISCO_HOME%\lib\opennlp\trove.jar;^
-%GATE_HOME%\bin\gate.jar;^
-%GATE_HOME%\lib\jdom.jar;^
-%GATE_HOME%\lib\xercesImpl.jar;^
-%GATE_HOME%\lib\jasper-compiler-jdt.jar;^
-%GATE_HOME%\lib\nekohtml-0.9.5.jar;^
-%GATE_HOME%\lib\ontotext.jar;^
-%GATE_HOME%\lib\stax-api-1.0.1.jar;^
-%GATE_HOME%\lib\PDFBox-0.7.2.jar ^
 relex.concurrent.ParallelRelationExtractor %1
