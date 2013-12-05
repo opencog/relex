6ff6caa4788d9fa65a9d3554b34d8a416f396d84
Linas Vepstas <linasvepstas@gmail.com>
2013-12-05 16:46:11 -0600

    redirect output to a server

--- src/java/relex/Server.java
+++ src/java/relex/Server.java
@@ -51,6 +51,7 @@ public class Server
 
 	public static void main(String[] args)
 	{
+		int host_port = 0;
 		int listen_port = 4444;
 		int max_parses = 1;
 		boolean relex_on = false;
@@ -58,10 +59,12 @@ public class Server
 		boolean anaphora_on = false;
 		boolean verbose = false;
 		String lang = "en";
+		String host_name = null;
 		String usageString = "RelEx server (designed for OpenCog interaction).\n" +
 			"Given a sentence, it returns a parse in opencog-style scheme format.\n" +
 			" -p number  \t Port number to listen on (default: 4444)\n" +
 			" --port num \t Port number to listen on (default: 4444)\n" +
+			" --host host:port\t Send output to indicated host:port\n" +
 			" --lang lang\t Set langauge (default: en)\n" +
 			" -n number  \t Max number of parses to return (default: 1)\n" +
 			" --relex    \t Output RelEx relations (default)\n" +
@@ -79,20 +82,31 @@ public class Server
 		HashSet<String> opts = new HashSet<String>();
 		opts.add("-n");
 		opts.add("-p");
+		opts.add("--host");
 		opts.add("--lang");
 		opts.add("--port");
 		Map<String,String> commandMap = CommandLineArgParser.parse(args, opts, flags);
 
 		try
 		{
-			String opt = commandMap.get("--lang");
-			if (opt != null) lang = opt;
+			String opt;
 
 			opt = commandMap.get("-n");
 			if (opt != null) max_parses = Integer.parseInt(opt);
 
 			opt = commandMap.get("-p");
 			if (opt != null) listen_port = Integer.parseInt(opt);
+
+			opt = commandMap.get("--host");
+			if (opt != null)
+			{
+				String[] hp = opt.split(":");
+				host_name = hp[0];
+				host_port = Integer.parseInt(hp[1]);
+			}
+
+			opt = commandMap.get("--lang");
+			if (opt != null) lang = opt;
 
 			opt = commandMap.get("--port");
 			if (opt != null) listen_port = Integer.parseInt(opt);
@@ -170,6 +184,22 @@ public class Server
 		}
 		System.err.println("Info: Listening on port " + s.listen_port);
 
+		// send output to some other place, rather than returning it.
+		Socket sendSocket = null;
+		if (host_name != null)
+		{
+			try
+			{
+				sendSocket = new Socket(host_name, host_port);
+			}
+			catch (Exception e)
+			{
+				System.err.println("Error: Unable to connect to " + 
+				   host_name + ":" + host_port + " : " + e.getMessage());
+				System.exit(-1);
+			}
+			System.err.println("Info: Will send output to " + host_name + ":" + host_port);
+		}
 		// -----------------------------------------------------------------
 		// Main loop
 		while(true)
