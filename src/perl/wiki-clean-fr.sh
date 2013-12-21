4b9978b8ab1dfd91ab9a83c4fc80cec289dd92d7
Linas Vepstas <linasvepstas@gmail.com>
2013-12-21 14:48:40 -0600

    work on the french cleanup

--- src/perl/wiki-clean-fr.sh
+++ src/perl/wiki-clean-fr.sh
@@ -6,37 +6,39 @@
 #
 # Copyright (c) 2008, 2013 Linas Vepstas <linas@linas.org>
 
+keep mediawiki
+
 Catégorie
-Wikipédia
 Fichier:
 Modèle
+Wikipédia
 
-echo "Category:"
-find . -name 'Category:*' -print | wc
+echo "Catégorie:"
+find . -name 'Catégorie:*' -print | wc
 find . -name 'MediaWiki:*' -print | wc
 find . -name 'Help:*' -print | wc
-echo "File:"
-find . -name 'File:*' -print | wc
+echo "Fichier:"
+find . -name 'Fichier:*' -print | wc
 find . -name 'Image:*' -print | wc
-echo "Template"
-find . -name 'Template:*' -print | wc
-find . -name 'Wikipedia:*' -print | wc
+echo "Modèle"
+find . -name 'Modèle:*' -print | wc
+find . -name 'Wikipédia:*' -print | wc
 find . -name '"List of "*' -print | wc
 find . -name '"Lists of "*' -print | wc
 
-# Must use "find" to accomplish this, since using "rm Category:*"
+# Must use "find" to accomplish this, since using "rm Catégorie:*"
 # leads to an overflow of the command line.
 
-echo "Category:"
-time find . -name 'Category:*' -exec rm {} \;
+echo "Catégorie:"
+time find . -name 'Catégorie:*' -exec rm {} \;
 time find . -name 'MediaWiki:*' -exec rm {} \;
 time find . -name 'Help:*' -exec rm {} \;
-# File: includes mp3's, ogg's, many different image types
-echo "File:"
-time find . -name 'File:*' -exec rm {} \;
+# Fichier: includes mp3's, ogg's, many different image types
+echo "Fichier:"
+time find . -name 'Fichier:*' -exec rm {} \;
 time find . -name 'Image:*' -exec rm {} \;
-echo "Template"
-time find . -name 'Template:*' -exec rm {} \;
-time find . -name 'Wikipedia:*' -exec rm {} \;
+echo "Modèle"
+time find . -name 'Modèle:*' -exec rm {} \;
+time find . -name 'Wikipédia:*' -exec rm {} \;
 time find . -name '"List of "*' -exec rm {} \;
 time find . -name '"Lists of "*' -exec rm {} \;
