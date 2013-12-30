faaede91382dbc498d14104056373a11ea0490bb
Amen Belayneh <amenbelayneh@gmail.com>
2013-12-30 09:37:54 +0300

    correction on relex2logic output

--- src/java/relex/logic/Rule.java
+++ src/java/relex/logic/Rule.java
@@ -128,11 +128,12 @@ public class Rule {
 			for (String variableName : criterium.getVariables()) {
 				String variableValue = criterium.getVariableValue(variableName);
                                 String variableValueUUID = criterium.getVariableValueUUID(variableName);
-                                
 				schemeOutput = schemeOutput.replaceAll(
-                                Pattern.quote(variableName), "\"" + variableValue + "\"");
+                                        Pattern.quote(variableName), "\"" + variableValue + "\"");
                                 schemeOutput = schemeOutput.replaceAll(
-                                Pattern.quote("word_index"), "\"" + variableValueUUID + "\"");
+                                        Pattern.quote("\"" + variableValue + "\"" + " (get-instance-name " + "\"" + variableValue + "\"" + " word_index"), 
+                                        "\"" + variableValue + "\"" + " (get-instance-name " + "\"" + variableValue + "\" " +"\"" + variableValueUUID + "\"");
+                                System.out.println(schemeOutput + "\n ---------------\n");
                                 
 			}
 		}
