7291d946c1f2caaae8b82e369e18acee25bd65fd
Rodas Solomon <rodisolomon@gmail.com>
2014-01-28 11:05:30 +0300

    add rule for who() relation for sentence with predicate adjective

--- relation-extractor.sh
+++ relation-extractor.sh
@@ -64,7 +64,8 @@ bin:\
 #/usr/lib/jvm/java-6-sun/bin/java $VM_OPTS $RELEX_OPTS $CLASSPATH relex.RelationExtractor -n 4 -l -t -r -a -s "Alice ate the mushroom."
 reset
 # java $VM_OPTS $RELEX_OPTS $CLASSPATH relex.RelationExtractor -n 4 -l -t -r -a -s "Alice ate the mushroom."
-java $VM_OPTS $RELEX_OPTS $CLASSPATH relex.RelationExtractor -n 4 -l -t -a --or --stanford --penn -s  "Pizza, which most people love, is not very healthy."
+java $VM_OPTS $RELEX_OPTS $CLASSPATH relex.RelationExtractor -n 4 -l -t -a --or --stanford --penn -s  "
+Anyone who is afraid of doing too much will end up doing to little."
 
 # Alternately, the sentence can be specified on the command line:
 # java $VM_OPTS $RELEX_OPTS $CLASSPATH relex.RelationExtractor -n 4 -l -t \
