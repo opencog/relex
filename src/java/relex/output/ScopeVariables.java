58c985cdb0afe63e7dd3aa6ca59bcf50d5e6f321
Rodas Solomon <rodisolomon@gmail.com>
2014-02-26 11:03:29 +0300

    correct indentation ScopeVariables.java

--- src/java/relex/output/ScopeVariables.java
+++ src/java/relex/output/ScopeVariables.java
@@ -28,45 +28,45 @@ import java.util.Scanner;
 public class ScopeVariables
 {
 
-    private final  String scopefile = "./data/varscope.txt";
-    private ArrayList<String> scope = new ArrayList();
+	private final  String scopefile = "./data/varscope.txt";
+	private ArrayList<String> scope = new ArrayList();
 
-    public ArrayList<String>  loadVarScope()
-    {
-        File file = new File(scopefile);
-        Scanner input = null;
-        try {
-            input = new Scanner(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        while (input.hasNext()) {
+	public ArrayList<String>  loadVarScope()
+	{
+		File file = new File(scopefile);
+		Scanner input = null;
+		try {
+			input = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		while (input.hasNext()) {
 
-            String nextLine = input.nextLine();
-            if (null == nextLine) break;
-            if (!nextLine.equals(""))
-                scope.add(nextLine);
+			String nextLine = input.nextLine();
+			if (null == nextLine) break;
+			if (!nextLine.equals(""))
+				scope.add(nextLine);
 
-        }
+		}
 
 
-        input.close();
+		input.close();
 
-        return getScope();
-    }
+		return getScope();
+	}
 
-    public  ArrayList<String> getScope()
-    {
-        ArrayList<String> sVar = new ArrayList<String>();
-        String temp[];
-        for( String  var: scope) {
-            temp = var.substring( (var.indexOf("[") + 1), (var.indexOf("]"))).split(",");
-            for( String  y : temp) {
-                sVar.add(y);
-            }
-        }
-        return sVar;
-    }
+	public  ArrayList<String> getScope()
+	{
+		ArrayList<String> sVar = new ArrayList<String>();
+		String temp[];
+		for( String  var: scope) {
+			temp = var.substring( (var.indexOf("[") + 1), (var.indexOf("]"))).split(",");
+			for( String  y : temp) {
+				sVar.add(y);
+			}
+		}
+		return sVar;
+	}
 
 }
 
