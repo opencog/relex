f0f429542f8dbc3afce54bd32fac17d0b9b09d4f
Amen Belayneh <amenbelayneh@gmail.com>
2014-07-03 21:03:40 +0800

    small change to aid in NLG tasks

--- src/java/relex/Server.java
+++ src/java/relex/Server.java
@@ -368,10 +368,16 @@ public class Server
 
 						if (logic_on)
 						{
+							out.println("; ##### START OF R2L #####");
 							out.println(logicView.printRelationsNew(parse));
 							out.flush();
 							System.err.println("Info: called relex2logic functions");
 						}
+
+						// This is for simplifying pre-processing of scheme string
+						// before evaluating it in opencog, for Relex2Logic.
+						out.println("; ##### END OF A PARSE #####");
+						out.flush();
 					}
 
 					// Add a special tag to tell the cog server that it's
