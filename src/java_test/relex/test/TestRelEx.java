9a7f40adf8e782a34c534574ba646379ccf8e761
Kasun Perera <kkasunperera@gmail.com>
2014-07-07 21:24:33 +0530

    add new sentence "I run less often than Ben does."

--- src/java_test/relex/test/TestRelEx.java
+++ src/java_test/relex/test/TestRelEx.java
@@ -356,6 +356,15 @@ public class TestRelEx
 				    "degree(often, comparative)\n"+
 				    "_advmod(often, more)\n");
 		
+		rc &= test_sentence ("I run less often than Ben does.",
+				    "_subj(run, I)\n"+
+				    "_subj(do, Ben)\n"+
+				    "_advmod(run, often)\n"+
+				    "_comparative(often, run)\n"+
+				    "than(I, Ben)\n"+
+				    "degree(often, comparative)\n"+
+				    "_advmod(often, less)\n");
+		
 		rc &= test_sentence ("I have more chairs than Ben.",
 				    "_obj(have, chair)\n"+
 				    "_subj(have, I)\n"+
