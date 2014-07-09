b02f8839422eaf1f7f14fea9d3d7b773c2942b2c
Kasun Perera <kkasunperera@gmail.com>
2014-07-09 22:30:25 +0530

    adding _advmod() relationship

--- src/java_test/relex/test/TestRelEx.java
+++ src/java_test/relex/test/TestRelEx.java
@@ -467,7 +467,7 @@ public class TestRelEx
 				    "_obj(run, race)\n"+
 				    "_subj(win, Ben)\n"+
 				    "_obj(win, contest)\n"+
-				    "_advmod(run, more)\n"+
+				    "_advmod(run, fewer)\n"+
 				    "_quantity(race, fewer)\n"+
 				    "_comparative(race, run)\n"+
 				    "than(I, Ben)\n"+
