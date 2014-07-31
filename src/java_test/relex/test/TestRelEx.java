cd34dd2c5a3c225ba047b63c33e4b91e2b567099
Kasun Perera <kkasunperera@gmail.com>
2014-07-31 22:29:41 +0530

    corrections to binary relationships

--- src/java_test/relex/test/TestRelEx.java
+++ src/java_test/relex/test/TestRelEx.java
@@ -878,7 +878,7 @@ public class TestRelEx
 		rc &= test_sentence ("Jack is more ingenious than Ben is crazy.", 
 				    "_predadj(Jack, ingenious)\n"+
 				    "_predadj(Ben, crazy)\n"+
-				    "_advmod(ingenious, less)\n"+
+				    "_advmod(ingenious, more)\n"+
 				    "_comparative(ingenious, Jack)\n"+
 				    "than(Jack, Ben)\n"+
 				    "than1(ingenious, crazy)\n"+
@@ -914,14 +914,14 @@ public class TestRelEx
 		rc &= test_sentence ("Amen's hair is as long as Ben's.",
 			"_poss(hair, Amen)\n"+
 			"_predadj(hair, long)\n"+
-			"_as(long, hair)\n"+
-			"_than(Amen, Ben)\n");
+			"as(long, hair)\n"+
+			"than(Amen, Ben)\n");
 
 		rc &= test_sentence ("Amen’s hair is same as Ben’s.",
 			"_poss(hair, Amen)\n"+
 			"_predadj(hair, same)\n"+
-			"_as(same, hair)\n"+
-			"_than(Amen, Ben)\n");
+			"as(same, hair)\n"+
+			"than(Amen, Ben)\n");
 
 		rc &= test_sentence ("Jack’s hair color is similar to that of Ben’s.",
 			"_poss(color, Jack)\n"+
@@ -929,26 +929,26 @@ public class TestRelEx
 			"_predadj(color, similar)\n"+
 			"of(that, Ben)\n"+
 			"to(similar, that)\n"+
-			"_as(similar, color)\n"+
-			"_than(Jack, Ben)\n");
+			"as(similar, color)\n"+
+			"than(Jack, Ben)\n");
 
 		rc &= test_sentence ("Jack is as intelligent as Ben.",
 			"_predadj(Jack, intelligent)\n"+
-			"_as(intelligent, Jack)\n"+
-			"_than(Jack, Ben)\n");
+			"as(intelligent, Jack)\n"+
+			"than(Jack, Ben)\n");
 
 		rc &= test_sentence ("The book’s color is same as that of the pen’s.",
 			"_poss(color, book)\n"+
 			"_predadj(color, same)\n"+
 			"of(that, pen)\n"+
-			"_as(same, color)\n"+
-			"_than(book, pen)\n");
+			"as(same, color)\n"+
+			"than(book, pen)\n");
 
 		rc &= test_sentence ("The snail is running  exactly as fast as the cheetah.",
 			"_predadj(snail, run)\n"+
-			"_as(fast, run)\n"+
+			"as(fast, run)\n"+
 			"_advmod(fast, exactly)\n"+
-			"_than(snail, cheetah)\n");
+			"than(snail, cheetah)\n");
 		
 		report(rc, "Equatives");
 		return rc;
