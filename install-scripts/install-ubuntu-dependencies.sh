136095aefa33c224ba6f5c2026153c7fabc55dcd
Larry Daniels <anitzkin@gmail.com>
2014-10-23 16:29:57 +0800

    Amen edited this so it works now; thought I'd share

--- install-scripts/install-ubuntu-dependencies.sh
+++ install-scripts/install-ubuntu-dependencies.sh
@@ -13,37 +13,38 @@
 #
 # Authors: David Hart, Cosmo Harrigan
 
-# Ubuntu/Debian Packages
-sudo apt-get -y update 
-sudo apt-get -y install build-essential python-dev swig zlib1g-dev unzip wget
-sudo apt-get -y install wordnet-dev wordnet-sense-index
-sudo apt-get -y install openjdk-7-jdk 
-sudo apt-get -y install ant libcommons-logging-java libgetopt-java
+## Ubuntu/Debian Packages
+#sudo apt-get -y update 
+#sudo apt-get -y install build-essential python-dev swig zlib1g-dev unzip wget
+#sudo apt-get -y install wordnet-dev wordnet-sense-index
+#sudo apt-get -y install openjdk-7-jdk 
+#sudo apt-get -y install ant libcommons-logging-java libgetopt-java
 
 # Link Grammar
-wget http://www.abisource.com/downloads/link-grammar/5.1.1/link-grammar-5.1.1.tar.gz
-tar -xvf link-grammar-5.1.1.tar.gz
-rm link-grammar-5.1.1.tar.gz
-cd link-grammar-5.1.1
+wget http://www.abisource.com/downloads/link-grammar/5.1.3/link-grammar-5.1.3.tar.gz
+tar -xvf link-grammar-5.1.3.tar.gz
+rm link-grammar-5.1.3.tar.gz
+cd link-grammar-5.1.3
 JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64  ./configure 
 make 
 sudo make install
 sudo ln -v -s /usr/local/lib/liblink-grammar.so.5 /usr/lib/liblink-grammar.so.5
 cd ..
+sudo ldconfig
 
 # Java WordNet Library
-wget http://downloads.sourceforge.net/project/jwordnet/jwnl/JWNL%201.4/jwnl14-rc2.zip
-unzip jwnl14-rc2.zip jwnl14-rc2/jwnl.jar
-sudo mv -v jwnl14-rc2/jwnl.jar /usr/local/share/java/
-rm -v jwnl14-rc2.zip && rmdir jwnl14-rc2
-sudo chmod -v 0644 /usr/local/share/java/jwnl.jar 
+#wget http://downloads.sourceforge.net/project/jwordnet/jwnl/JWNL%201.4/jwnl14-rc2.zip
+#unzip jwnl14-rc2.zip jwnl14-rc2/jwnl.jar
+#sudo mv -v jwnl14-rc2/jwnl.jar /usr/local/share/java/
+#rm -v jwnl14-rc2.zip && rmdir jwnl14-rc2
+#sudo chmod -v 0644 /usr/local/share/java/jwnl.jar 
 
 # RelEx
-if grep -q '^vagrant:' /etc/passwd; then
-    cd /home/vagrant/relex
-    sudo -u vagrant ant build
-else
-    ant build
-fi
+#if grep -q '^vagrant:' /etc/passwd; then
+#    cd /home/vagrant/relex
+#    sudo -u vagrant ant build
+#else
+#    ant build
+#fi
 
-sudo ant install
+#sudo ant install
