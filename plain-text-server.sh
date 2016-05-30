a39f3605d22d6ff710bc02dd2c6cce10dbbcfd00
mjsduncan <mjsduncan@gmail.com>
2016-05-30 18:40:33 -0400

    Update plain-text-server.sh
    add .jar paths to match build.xml
    /usr/local/share/java/opennlp-tools-1.5.3.jar:\
    /usr/local/share/java/maxent-3.0.3.jar:\

--- plain-text-server.sh
+++ plain-text-server.sh
@@ -26,6 +26,8 @@ RELEX_OPTS="\
 
 CLASSPATH="-classpath \
 bin:\
+/usr/local/share/java/opennlp-tools-1.5.3.jar:\
+/usr/local/share/java/maxent-3.0.3.jar:\
 /usr/local/share/java/opennlp-tools-1.5.0.jar:\
 /usr/local/share/java/maxent-3.0.0.jar:\
 /usr/local/share/java/maxent-2.5.2.jar:\
