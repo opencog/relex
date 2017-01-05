9ef1169b807043c544d2f05fcdd35c19a4b4ca33
Linas Vepstas <linasvepstas@gmail.com>
2017-01-05 10:26:07 -0600

    More general cleanup.

--- src/split-sentences/split-sentences.pl
+++ src/split-sentences/split-sentences.pl
@@ -46,7 +46,7 @@ if (!$QUIET) {
 
 my $prefixfile = "$mydir/nonbreaking_prefix.$language";
 
-#default back to English if we don't have a language-specific prefix file
+# Default to English, if we don't have a language-specific prefix file.
 if (!(-e $prefixfile)) {
 	$prefixfile = "$mydir/nonbreaking_prefix.en";
 	print STDERR "WARNING: No known abbreviations for language '$language', attempting fall-back to English version...\n";
@@ -74,13 +74,13 @@ my $text = "";
 while (<STDIN>) {
 	chop;
 	if (/^<.+>$/ || /^\s*$/) {
-		#time to process this block, we've hit a blank or <p>
-		&do_it_for($text,$_);
-		print "<P>\n" if (/^\s*$/ && $text); ##if we have text followed by <P>
+		# Time to process this block; we've hit a blank or <p>
+		&do_it_for($text, $_);
+		print "<P>\n" if (/^\s*$/ && $text); ## If we have text followed by <P>
 		$text = "";
 	}
 	else {
-		#append the text, with a space
+		# Append the text, with a space.
 		$text .= $_. " ";
 	}
 }
@@ -124,31 +124,33 @@ sub preprocess {
 	# and are followed by a sentence starter punctuation and upper case.
 	$text =~ s/([?!\.]) +([\'\"\(\[\¿\¡\p{IsPi}]+[\ ]*[\p{IsUpper}])/$1\n$2/g;
 
-	# special punctuation cases are covered. Check all remaining periods.
+	# Special punctuation cases are covered. Check all remaining periods.
 	my $word;
 	my $i;
 	my @words = split(/ /,$text);
 	$text = "";
 	for ($i=0;$i<(scalar(@words)-1);$i++) {
 		if ($words[$i] =~ /([\p{IsAlnum}\.\-]*)([\'\"\)\]\%\p{IsPf}]*)(\.+)$/) {
-			#check if $1 is a known honorific and $2 is empty, never break
+			# Check if $1 is a known honorific and $2 is empty, never break.
 			my $prefix = $1;
 			my $starting_punct = $2;
-			if($prefix && $NONBREAKING_PREFIX{$prefix} && $NONBREAKING_PREFIX{$prefix} == 1 && !$starting_punct) {
-				#not breaking;
+			if ($prefix && $NONBREAKING_PREFIX{$prefix} && $NONBREAKING_PREFIX{$prefix} == 1 && !$starting_punct) {
+				# Not breaking;
 			} elsif ($words[$i] =~ /(\.)[\p{IsUpper}\-]+(\.+)$/) {
-				#not breaking - upper case acronym
+				# Not breaking - upper case acronym
 			} elsif($words[$i+1] =~ /^([ ]*[\'\"\(\[\¿\¡\p{IsPi}]*[ ]*[\p{IsUpper}0-9])/) {
 				# The next word has a bunch of initial quotes, maybe a
 				# space, then either upper case or a number
 				$words[$i] = $words[$i]."\n" unless ($prefix && $NONBREAKING_PREFIX{$prefix} && $NONBREAKING_PREFIX{$prefix} == 2 && !$starting_punct && ($words[$i+1] =~ /^[0-9]+/));
-				#we always add a return for these unless we have a numeric non-breaker and a number start
+				# We always add a return for these, unless we have a
+				# numeric non-breaker and a number start.
 			}
 		}
 		$text = $text.$words[$i]." ";
 	}
 
-	# We stopped one token from the end to allow for easy look-ahead. Append it now.
+	# We stopped one token from the end to allow for easy look-ahead.
+	# Append it now.
 	$text = $text.$words[$i];
 
 	# Clean up spaces at head and tail of each line as well as any double-spacing
